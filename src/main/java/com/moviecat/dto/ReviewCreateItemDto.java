c4997e74634732681fc249f151ff884a967b218b
kiribu <kiryshabutor@gmail.com>
2026-03-23 18:23:04 +0300

    lab5/fix sonar dublications

--- src/main/java/com/moviecat/dto/ReviewCreateItemDto.java
+++ src/main/java/com/moviecat/dto/ReviewCreateItemDto.java
@@ -1,42 +1,13 @@
 package com.moviecat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Schema(name = "ReviewCreateItem", description = "Review item for bulk creation")
-public class ReviewCreateItemDto {
+public class ReviewCreateItemDto extends ReviewContentDto {
 
-    @NotBlank(message = "Author alias is required")
-    @Schema(
-            description = "Author alias",
-            example = "Critic42",
-            minLength = 1,
-            maxLength = 80,
-            requiredMode = Schema.RequiredMode.REQUIRED)
-    private String authorAlias;
-
-    @NotNull(message = "Rating is required")
-    @Min(value = 1, message = "Rating must be between 1 and 10")
-    @Max(value = 10, message = "Rating must be between 1 and 10")
-    @Schema(
-            description = "Rating from 1 to 10",
-            example = "9",
-            minimum = "1",
-            maximum = "10",
-            requiredMode = Schema.RequiredMode.REQUIRED)
-    private Integer rating;
-
-    @Schema(description = "Review comment", example = "Great movie!", maxLength = 500)
-    private String comment;
+    public ReviewCreateItemDto(String authorAlias, Integer rating, String comment) {
+        super(authorAlias, rating, comment);
+    }
 }
--- src/main/java/com/moviecat/dto/ReviewDto.java
+++ src/main/java/com/moviecat/dto/ReviewDto.java
@@ -1,11 +1,7 @@
 package com.moviecat.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,37 +9,13 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Schema(name = "ReviewPayload", description = "Review request/response payload")
-public class ReviewDto {
+public class ReviewDto extends ReviewContentDto {
     @Schema(
             description = "Review ID",
             example = "1",
             accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
-
-    @NotBlank(message = "Author alias is required")
-    @Schema(
-            description = "Author alias",
-            example = "Critic42",
-            minLength = 1,
-            maxLength = 80,
-            requiredMode = Schema.RequiredMode.REQUIRED)
-    private String authorAlias;
-
-    @NotNull(message = "Rating is required")
-    @Min(value = 1, message = "Rating must be between 1 and 10")
-    @Max(value = 10, message = "Rating must be between 1 and 10")
-    @Schema(
-            description = "Rating from 1 to 10",
-            example = "9",
-            minimum = "1",
-            maximum = "10",
-            requiredMode = Schema.RequiredMode.REQUIRED)
-    private Integer rating;
-
-    @Schema(description = "Review comment", example = "Great movie!", maxLength = 500)
-    private String comment;
 
     @NotNull(message = "Movie ID is required")
     @Schema(
@@ -52,4 +24,10 @@ public class ReviewDto {
             minimum = "1",
             requiredMode = Schema.RequiredMode.REQUIRED)
     private Long movieId;
+
+    public ReviewDto(Long id, String authorAlias, Integer rating, String comment, Long movieId) {
+        super(authorAlias, rating, comment);
+        this.id = id;
+        this.movieId = movieId;
+    }
 }
